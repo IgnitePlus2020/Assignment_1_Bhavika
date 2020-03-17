a3c96931b9753ea881c0f9a4d69f7a39c00308e9
Bhavika007 <57570012+Bhavika007@users.noreply.github.com>
2020-03-17 20:04:04 +0530

    Update frac_whole.java

--- frac_whole.java
+++ frac_whole.java
@@ -2,7 +2,9 @@ package com.tgt.igniteplus;
 import java.util.*;
 public class frac_whole {
     public static void main(String[] args) {
-        double value = 12.56;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        double value = in.nextDouble();
         double fractional_part = value % 1;
         double whole_part = value - fractional_part;
         System.out.print("\nOriginal value: "+value);
